60d661c64fa5574318424828ac8fd5f2d24186f3
Rizal Danu <admin@rizaldanu.web.id>
2018-06-11 13:58:30 +0700

    mon

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('PSM_DB_HOST', '172.30.129.138');
+define('PSM_DB_PORT', '3306');
+define('PSM_DB_NAME', 'sampledb');
+define('PSM_DB_USER', 'danu');
+define('PSM_DB_PASS', 'bmroot');
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_BASE_URL', 'http://mon-danu.7e14.starter-us-west-2.openshiftapps.com');
+
